ee13f2fd7e749ea3661ad39e28a306beee240cda
baibin <2487308772@qq.com>
2022-07-04 02:06:22 +0800

    error-commit-001

---
+++ demo-maven-son01/src/test/java/com/baibin/maven/errortest.java
@@ -0,0 +1,15 @@
+package com.baibin.maven;
+
+/**
+ * @author baibin
+ * @date 2022/7/4 - 2:05
+ */
+public class errortest {
+    public static void main(String[] args) {
+        System.out.println("error-test");
+        System.out.println("error-test");
+        System.out.println("error-test");
+        System.out.println("error-test");
+        System.out.println("error-test");
+    }
+}
