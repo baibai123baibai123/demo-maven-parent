2d0d79123287817ad208d1ebb366b4516096dded
Xiaobai <1359023685@qq.com>
2022-07-03 23:19:51 +0800

    hot-commit

--- demo-maven-son01/src/test/java/com/baibin/maven/HelloTest.java
+++ demo-maven-son01/src/test/java/com/baibin/maven/HelloTest.java
@@ -14,6 +14,7 @@ public class HelloTest {
         System.out.println("22222222222222222222222");
         System.out.println("333333333333333333333333");
         System.out.println("4444444444444444");
+        System.out.println("hot-test");
     }
 
 }
