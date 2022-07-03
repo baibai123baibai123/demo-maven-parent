4a57a009e7571e68af1ac299d9f4a25fb37a095e
baibin <2487308772@qq.com>
2022-07-04 02:13:11 +0800

    error-commit-001

---
+++ demo-maven-son01/src/test/java/com/baibin/maven/test01.java
@@ -0,0 +1,11 @@
+package com.baibin.maven;
+
+/**
+ * @author baibin
+ * @date 2022/7/4 - 2:10
+ */
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111");
+    }
+}
