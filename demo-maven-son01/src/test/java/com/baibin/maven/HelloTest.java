22a592c697daac8ecec14eda8a19cbe69e2bff58
Xiaobai <1359023685@qq.com>
2022-07-03 23:10:16 +0800

    hot-fix-1-commit

--- demo-maven-son01/src/test/java/com/baibin/maven/HelloTest.java
+++ demo-maven-son01/src/test/java/com/baibin/maven/HelloTest.java
@@ -13,6 +13,7 @@ public class HelloTest {
         hello.show();
         System.out.println("22222222222222222222222");
         System.out.println("333333333333333333333333");
+        System.out.println("4444444444444444");
     }
 
 }
